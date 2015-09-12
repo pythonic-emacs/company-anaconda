137ba58960a5feeb23aff34c9225fdf161f49f98
Artem Malyshev <proofit404@gmail.com>
2015-09-12 11:06:52 +0300

    Use latest anaconda-mode version.

--- company-anaconda.el
+++ company-anaconda.el
@@ -5,7 +5,7 @@
 ;; Author: Artem Malyshev <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/anaconda-mode
 ;; Version: 0.1.0
-;; Package-Requires: ((company "0.8.0") (anaconda-mode "0.1.0") (cl-lib "0.5.0") (dash "2.6.0") (s "1.9"))
+;; Package-Requires: ((company "0.8.0") (anaconda-mode "0.1.1") (cl-lib "0.5.0") (dash "2.6.0") (s "1.9"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
